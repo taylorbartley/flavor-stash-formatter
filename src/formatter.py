566351570a448d0fd3f0fb2379b952a4a46c89ab
taylorbartley <39292007+taylorbartley@users.noreply.github.com>
2019-09-08 01:40:04 -0500

    date appending to filename. bugfix where a comma in a name would shift the row

--- src/formatter.py
+++ src/formatter.py
@@ -3,7 +3,7 @@
 #!/usr/bin/env python3
 
 import csv
-from datetime import datetime
+from datetime import date
 
 
 def format_row(row):
@@ -12,6 +12,8 @@ def format_row(row):
     for item in row:
         if not item.strip():
             out_row.append("n/a")
+        elif ", " in item:
+            out_row.append(item.replace(', ', '--'))
         else:
             out_row.append(item)
     return out_row
@@ -19,17 +21,20 @@ def format_row(row):
 
 def main():
     """Go."""
+    today = date.today().strftime("%Y-%m-%d")
+
     row_list = []
-    in_file = "stash_report.csv"
-    out_file = "out_report.csv"
+    in_file = "reports/stash_report.csv"
+    out_file = f"reports/out_report-{today}.csv"
     with open(in_file, newline="") as file:
         reader = csv.reader(file, delimiter=";")
         for row in reader:
+            print(", ".join(format_row(row)))
             row_list.append(", ".join(format_row(row)))
 
     with open(out_file, "w", newline="") as file:
 
-        writer = csv.writer(file, delimiter=";")
+        writer = csv.writer(file, delimiter=';')
         for row in row_list.copy():
             writer.writerow([row])
 
